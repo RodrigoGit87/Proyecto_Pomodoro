8d5b0fd886d84e2eca31e70f59fcf3e4cd027d93
RodrigoGit87 <rodrigollamas87@gmail.com>
2026-03-24 21:00:51 +0100

    Recogo elementos, creo banderas bool.

---
+++ scripts/pomodoro.js
@@ -0,0 +1,18 @@
+//Recogo los elementos
+const reloj = document.getElementById("reloj");
+const counter= document.getElementById("counter");
+const minutos=document.getElementById("minutos");
+const segundos=document.getElementById("segundos");
+const start=document.getElementById("start");
+const stop=document.getElementById("stop");
+
+//interruptor booleano
+let tiempo_ciclo =false;
+let tiempo_descanso = false;
+
+start.addEventListener("click", ()=>{
+ //To DO
+
+});
+
+

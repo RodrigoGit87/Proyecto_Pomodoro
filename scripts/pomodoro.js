2dc7695efc579e3e8649e6d1c1d1034b5d2367e1
RodrigoGit87 <rodrigollamas87@gmail.com>
2026-03-25 13:10:40 +0100

    Funcion cuentaatras, start y stop listeners

--- scripts/pomodoro.js
+++ scripts/pomodoro.js
@@ -9,10 +9,39 @@ const stop=document.getElementById("stop");
 //interruptor booleano
 let tiempo_ciclo =false;
 let tiempo_descanso = false;
+//variables numericas
+let minutos_int=25;
+let segundos_int=0;
+let intervalo;
+let en_marcha=false;
 
+//Funcion para cuenta atrrás
+function cuenta_atras(){
+    if(segundos_int > 0) segundos_int--;
+    else if(minutos_int>0){
+        segundos_int=59;
+        minutos_int--;
+    } else {
+        alert(`Ciclo de trabajo completado`);
+        clearInterval(intervalo);
+        en_marcha=false;
+        return;
+    } 
+
+    minutos.innerText = minutos_int<10 ? "0"+minutos_int : minutos_int
+    segundos.innerText=segundos_int<10 ? "0"+segundos_int : segundos_int
+}
+
+//iniciar el reloj
 start.addEventListener("click", ()=>{
- //To DO
-
+    if(en_marcha==false){
+        en_marcha=true;
+        //llamar a la funcion q empieza la cuenta atrás
+        intervalo=setInterval(cuenta_atras, 1000)
+    }
 });
-
-
+//parar el reloj
+stop.addEventListener("click", () => {
+    clearInterval(intervalo);
+    en_marcha=false;
+})
